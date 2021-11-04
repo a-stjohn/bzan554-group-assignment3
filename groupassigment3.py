c6d05ca9c9a88195634961c4d34e1fbe74f61557
a-stjohn <aaron.m.stjohn@gmail.com>
2021-11-04 13:17:19 -0400

    Add the assignment file

---
+++ groupassigment3.py
@@ -0,0 +1,10 @@
+#Group assignment 3
+
+#Use the data, inputs and outputs as specified in group assignment 2.
+
+#Given everything we have learned, compare the performance of a feed-forward neural network and a LSTM. 
+
+#Answer:
+
+
+
